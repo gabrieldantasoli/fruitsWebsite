7c11204992afee474ba3d658567f0a6ebd135c17
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-01-10 19:01:33 -0300

    Adding new elements and updating files

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
+let previewContainer = document.querySelector('.products-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
+
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-//MAKE SCROLL
+document.querySelectorAll('.box-container .prod').forEach(product => {
+    product.onclick = () => {
+        previewContainer.style.display = 'flex';
+        let name = product.getAttribute('data-name');
+        previewBox.forEach(preview => {
+            let target = preview.getAttribute('data-target');
+            if (name == target){
+                preview.classList.add('active')
+            }
+        })
+    }
+})
+
+previewBox.forEach(close => {
+    close.querySelector('.fa-times').onclick = () => {
+        close.classList.remove('active');
+        previewContainer.style.display = 'none';
+    }
+})
